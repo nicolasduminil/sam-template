273fb41122145dc9e38c24c8fcaefb672558cf52
Nicolas DUMINIL <nicolas.duminil@simplex-software.fr>
2023-01-03 17:35:41 +0100

    Update {{ cookiecutter.java_class_name }}.java

--- {{ cookiecutter.project_name }}/src/main/java/{{ cookiecutter.java_package_name }}/{{ cookiecutter.java_class_name }}.java
+++ {{ cookiecutter.project_name }}/src/main/java/{{ cookiecutter.java_package_name }}/{{ cookiecutter.java_class_name }}.java
@@ -2,8 +2,16 @@ package {{ cookiecutter.java_package_name }};
 
 public class {{ cookiecutter.java_class_name }} 
 {
-  public String {{ cookiecutter.java_handler_method_name }} () 
+  private static String message = System.getenv("message");
+
+  public {{ cookiecutter.java_class_name }}() 
   {
-    return "Hello";
+    System.out.printf("*** {{ cookiecutter.java_class_name }}: Have initialized with configuration: %s", message);
+  }
+
+  public String {{ cookiecutter.java_handler_method_name }} (Map<String, String> event) 
+  {
+    System.out.println("*** {{ cookiecutter.java_class_name }}.{{ cookiecutter.java_handler_method_name }}: Have received: " + event);
+    return event.entrySet().stream().map(e -> e.getKey() + "->" + e.getValue()).collect(Collectors.joining(","));
   }
 }
