b3f60f7e61d390eddefc34c84142bb631b4332f3
Nicolas DUMINIL <nicolas.duminil@simplex-software.fr>
2023-01-04 16:00:36 +0100

    Update {{ cookiecutter.java_class_name }}.java

--- {{ cookiecutter.project_name }}/src/main/java/{{ cookiecutter.java_package_name }}/{{ cookiecutter.java_class_name }}.java
+++ {{ cookiecutter.project_name }}/src/main/java/{{ cookiecutter.java_package_name }}/{{ cookiecutter.java_class_name }}.java
@@ -2,19 +2,21 @@ package {{ cookiecutter.java_package_name }};
 
 import java.util.Map;
 import java.util.stream.Collectors;
+import java.util.logging.Logger;
 
 public class {{ cookiecutter.java_class_name }} 
 {
   private static String message = System.getenv("message");
+  private static Logger log = Logger.getLogger({{ cookiecutter.java_class_name }}.class.getName());
 
   public {{ cookiecutter.java_class_name }}() 
   {
-    System.out.printf("*** {{ cookiecutter.java_class_name }}: Have initialized with configuration: %s", message);
+    log.info("*** AwsLambdaSimple: Have initialized with environment: " + message);
   }
 
   public String {{ cookiecutter.java_handler_method_name }} (Map<String, String> event) 
   {
-    System.out.println("*** {{ cookiecutter.java_class_name }}.{{ cookiecutter.java_handler_method_name }}: Have received: " + event);
+    log.info("*** AwsLambdaSimple.handleRequest: Have received: " + event);
     return event.entrySet().stream().map(e -> e.getKey() + "->" + e.getValue()).collect(Collectors.joining(","));
   }
 }
